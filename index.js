d464b8a38e2b7a1d2c9ef013597ba3f37aa985e1
Celia Marshall <celia.m@gmail.com>
2018-09-27 12:06:25 -0700

    examples of working with file systems

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+const path = require('path')
+
+const fs = require('fs')
+
+const directoryName = __dirname
+
+const filePath = path.join(__dirname, 'classmates.txt')
+
+const classmates = fs.readFileSync(filePath, 'utf-8')
+
+const toSave = `${classmates} was here`
+
+//Overwriting the file:
+//fs.writeFileSync(filePath, 'Was here', 'utf-8')
+
+fs.writeFileSync(filePath, toSave, 'utf-8')
